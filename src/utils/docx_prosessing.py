1027b8e97081ca69a21e4aebb55d383c20bae475
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-12 13:13:32 +0200

    parse the placeholders

--- src/utils/docx_prosessing.py
+++ src/utils/docx_prosessing.py
@@ -1,8 +1,11 @@
+import io
 import re
+import streamlit as st
 from docx import Document
+from src.utils.field_mappings import get_unique_keys
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
-def process_doc(uploaded_file: UploadedFile) -> list[str]:
+def process_doc(uploaded_file: UploadedFile) -> None:
     doc = Document(uploaded_file)
     fields_to_fill = []
 
@@ -10,9 +13,23 @@ def process_doc(uploaded_file: UploadedFile) -> list[str]:
     # so this is enough parsing
     
     pattern = r"\{\{(\w+)\}\}"
+
+    # Tables
+
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for para in cell.paragraphs:
+                    matches = re.findall(pattern, para.text)
+                    fields_to_fill.extend(matches)
+
+    # Paragraphs
     
     for para in doc.paragraphs:
         matches = re.findall(pattern, para.text)
         fields_to_fill.extend(matches)
     
-    return fields_to_fill
+    st.session_state["field_mappings"] = get_unique_keys(fields_to_fill)
+
+    return
+
