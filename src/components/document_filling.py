de43302faaf6f19017b65d5eec935c758b0d3234
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-12 12:41:05 +0200

    Add input forms for found fields

--- src/components/document_filling.py
+++ src/components/document_filling.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from src.utils.docx_prosessing import process_doc
 
-def render_document_filling_UI():
-    st.info("You have successfully uploaded a document!")
+
+def render_document_filling_UI() -> None:
+    uploaded_file = st.session_state.get("uploaded_file")
+    if uploaded_file:
+        fields_to_fill = process_doc(uploaded_file)
+        for field in fields_to_fill:
+            st.text_input(label=field, key=field)
+            
