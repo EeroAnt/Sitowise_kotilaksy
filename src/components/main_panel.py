ab9829a12be3295c20c8c7b8a7fa9a11786be423
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-12 12:40:16 +0200

    Add return types

--- src/components/main_panel.py
+++ src/components/main_panel.py
@@ -3,7 +3,7 @@ from src.components.document_filling import render_document_filling_UI
 from src.components.file_upload import render_file_upload
 
 
-def render_main_panel():
+def render_main_panel() -> None:
     if not st.session_state.get("file_uploaded", None):
         render_file_upload()
     else:
