c9f886e55e348ecd03c2e4f088123c16243401c0
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-12 15:53:48 +0200

    Readd the mistakengly removed line and add comments

--- src/components/document_filling.py
+++ src/components/document_filling.py
@@ -6,12 +6,18 @@ def render_document_filling_UI() -> None:
     uploaded_file = st.session_state.get("uploaded_file")
     if uploaded_file:
         process_doc(uploaded_file)
+        # Render the input forms
         for field, key in st.session_state["field_mappings"]:
             st.text_input(label=field, key=key)
+
+        # Button for initial document creation
         if not st.session_state.get("submitted", None):
             if st.button("Submit", type="primary"):
+                st.session_state["filled_document"] = create_filled_doc(uploaded_file, st.session_state["field_mappings"])
                 st.session_state["submitted"] = True
                 st.rerun()
+
+        # Once created, present the download button and button to refresh the contents of the new doc
         if st.session_state.get("submitted", None):
             st.download_button(
                 label="Download filled document",
@@ -19,7 +25,7 @@ def render_document_filling_UI() -> None:
                 file_name="filled_document.docx",
                 mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
             )
-            if st.button("Edit again"):
+            if st.button("Refresh contents"):
                 st.session_state["filled_document"] = create_filled_doc(uploaded_file, st.session_state["field_mappings"])
                 st.info("Document content refreshed!")
                 st.rerun()
